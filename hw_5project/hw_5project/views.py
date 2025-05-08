f45c35c2a3f5b105a6b3733ced05285ed79e1627
Muslimbek <akimzhanovmuslimbek@gmail.com>
2025-05-08 13:20:35 +0600

    Remove unnecessary folders

--- hw_5project/hw_5project/views.py
+++
@@ -1,6 +0,0 @@
-# hw_5project/views.py
-
-from django.shortcuts import render  # Импортируем render для отображения шаблонов
-
-def home(request):
-    return render(request, 'home.html')  # Отображаем шаблон 'home.html' на главной странице
